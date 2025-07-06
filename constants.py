12a1b108fa6924b8a9540eb97f01f51503e92155
Noam Zamski <nzamski1@gmail.com>
2025-07-06 20:34:20 +0300

    Remove unused NAME_SIM_THRESHOLD constant from constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 DATE_COLUMNS = ['Date Modified', 'Date Added', 'Last Played', 'Last Skipped']
-NAME_SIM_THRESHOLD = 0.8
 KEY_COLUMNS = ['Name', 'Artist']
 
 PLAYLIST_DIR = Path('playlists')
